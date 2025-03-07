4dc54e7460cb5c77160816b1fd77e8759755c9ec
RohitBehera25 <153441363+RohitBehera25@users.noreply.github.com>
2025-03-07 02:19:03 +0000

    Converstion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,49 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);  //Op : NaN
+
+/*
+console.log(valueInNumber); // O/p : If The Value is null then the O/p will be 0
+console.log(valueInNumber); // O/p : If the value is undefined the O/p will be NaN
+console.log(valueInNumber); // O/p : If the value is true(boolean value) the O/p will be 1
+console.log(valueInNumber); // O/p : If the value is false(boolean value) the O/p will be 0
+console.log(valueInNumber); // O/p : If the value is string(Rohit) the O/p will be NaN
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);  //O:p True
+
+let isLoggedOut = 0
+
+let booleanIsLoggedOut = Boolean(isLoggedOut)
+// console.log(booleanIsLoggedOut); //O:p False
+
+let is = ""
+let aIs = Boolean(is)
+// console.log(aIs);  //O:p False
+
+let aa = "Rohit"
+let bAA = Boolean(aa)
+// console.log(bAA); //O:p True
+
+let someNum = 33
+let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum); //O/p : String
+
+
+
+
+
+
+
+
