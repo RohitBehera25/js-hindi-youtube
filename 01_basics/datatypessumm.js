18ad3ea27d3cff1e96dc2c380bbfe80eace42587
RohitBehera25 <153441363+RohitBehera25@users.noreply.github.com>
2025-03-11 04:14:52 +0000

    summary of datatypes

---
+++ 01_basics/datatypessumm.js
@@ -0,0 +1,39 @@
+//  Primitive (Call-By-Value) which means Jaab hi ham usko kahi na kahi se copy karta hai jo 
+// uska orginal data ka refernce nhi diya jaat uska copy kar ka diya jaat hai or 
+// usma jo bhi changes karta ho uska copy mai karta ho 
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
